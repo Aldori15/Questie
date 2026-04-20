b9bdc1439e22311c602b62058a3bc3bfc32d6fb8
Aldori <m.baxter1122@yahoo.com>
2026-04-19 20:55:03 -0400

    fix: Questie world map button while using Magnify
    In addition to the quest icons that this PR fixed, during testing I noticed that Questie's world map button at the top right of the world map was also not remaining in the correct spot while zooming with Magnify.  This will correct that.

--- Modules/WorldMapButton/WorldMapButton.lua
+++ Modules/WorldMapButton/WorldMapButton.lua
@@ -18,6 +18,15 @@ local mapButton
 function WorldMapButton.Initialize()
     mapButton = KButtons:Add("QuestieWorldMapButtonTemplate", "BUTTON")
 	WorldMapButton.Toggle(Questie.db.profile.mapShowHideEnabled)
+	local worldMapButtonFrame = _G.WorldMapButton
+
+    if worldMapButtonFrame then
+        mapButton:SetParent(worldMapButtonFrame)
+        mapButton:ClearAllPoints()
+        mapButton:SetFrameStrata("TOOLTIP")
+        mapButton:SetFrameLevel(worldMapButtonFrame:GetFrameLevel() + 1)
+        mapButton:SetPoint("TOPRIGHT", worldMapButtonFrame, "TOPRIGHT", -4, -4)
+    end
 
     Questie.WorldMap = {
         Button = mapButton
