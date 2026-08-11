f8c333aa3644dc8331d37198847b5a852c41148f
Aldori <m.baxter1122@yahoo.com>
2026-08-11 10:44:01 -0400

    fix(map): icon glow not adjusting to icon scale until next refresh

--- Modules/Map/QuestieMapUtils.lua
+++ Modules/Map/QuestieMapUtils.lua
@@ -132,6 +132,9 @@ function QuestieMap.utils:RescaleIcon(frameRef, mapScale)
 
             if scale > 1 then
                 frame:SetSize(scale * ZOOM_MODIFIER, scale * ZOOM_MODIFIER);
+                if frame.GlowUpdate then
+                    frame:GlowUpdate()
+                end
             end
         else
             Questie:Error("A frame is lacking the GetIconScale function for resizing!", frame.data.Id);
