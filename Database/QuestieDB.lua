d56039ba8658dea41267b79b916610f7fe2a629c
Aldori <m.baxter1122@yahoo.com>
2026-08-23 19:02:34 -0400

    fix(tooltips): show levels for source item NPCs

--- Database/QuestieDB.lua
+++ Database/QuestieDB.lua
@@ -3996,6 +3996,14 @@ function QuestieDB:GetCreatureLevels(quest)
             end
         end
     end
+    if quest.requiredSourceItems then
+        for _, itemId in pairs(quest.requiredSourceItems) do
+            local npcIds = QuestieDB.QueryItemSingle(itemId, "npcDrops")
+            if npcIds then
+                _CollectCreatureLevels(npcIds)
+            end
+        end
+    end
     if quest.Id then
         QuestieDB._CreatureLevelCache[quest.Id] = creatureLevels
     end
