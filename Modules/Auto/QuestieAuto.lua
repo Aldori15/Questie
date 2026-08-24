a2d78cecb909d94eb8cf8a6a62133ac94fe10bfe
Aldori <m.baxter1122@yahoo.com>
2026-08-24 00:25:07 -0400

    move QuestieCompat.Is335 check

--- Modules/Auto/QuestieAuto.lua
+++ Modules/Auto/QuestieAuto.lua
@@ -366,11 +366,7 @@ function QuestieAuto.GOSSIP_CLOSED()
 end
 
 function QuestieAuto.QUEST_ACCEPTED()
-    if QuestieCompat.Is335 then
-        return
-    end
-
-    if Questie.db.profile.bugWorkarounds == true and QuestFrameDetailPanel and QuestFrameDetailPanel:IsVisible() and QuestFrameCloseButton then
+    if not QuestieCompat.Is335 and Questie.db.profile.bugWorkarounds == true and QuestFrameDetailPanel and QuestFrameDetailPanel:IsVisible() and QuestFrameCloseButton then
         QuestFrameCloseButton:Click()
     end
 end
